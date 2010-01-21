5618b752194c31935cf4a3c7082ea0d66dc767b6
Rizwan Reza <rizwanreza@gmail.com>
2010-01-21 23:04:36 +0330

    Added committers name and links

--- leaves/lighthouse/controller.rb
+++ leaves/lighthouse/controller.rb
@@ -16,7 +16,39 @@ class Controller < Autumn::Leaf
     end
   end
   
-  def wycats_command(stem,sender,reply_to,msg)
+  def wycats_command(stem, sender, reply_to, msg)
     "Yehuda Katz: http://yehudakatz.com/"
+  end
+
+  def dhh_command(stem, sender, reply_to, msg)
+    "David Heinemeier Hansson: http://loudthinking.com/"
+  end
+
+  def lifo_command(stem, sender, reply_to, msg)
+    "Pratik Naik: http://m.onkey.org/"
+  end
+
+  def josevalim_command(stem, sender, reply_to, msg)
+    "José Valim: http://yehudakatz.com/"
+  end
+  
+  def carllerche_command(stem, sender, reply_to, msg)
+    "Carl Lerche: http://carllerche.com/"
+  end
+  
+  def nzkoz_command(stem, sender, reply_to, msg)
+    "Michael Koziarski: http://www.koziarski.com/"
+  end
+    
+  def rizwanreza_command(stem, sender, reply_to, msg)
+    "Rizwan Reza: http://rizwanreza.com/"
+  end
+
+  def sikachu_command(stem, sender, reply_to, msg)
+    "Sikachu: http://sikachu.com/"
+  end
+
+  def bugmash_command(stem, sender, reply_to, msg)
+    "Bugmash isn't going on, keep checking!"
   end
 end
